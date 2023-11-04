ce905363255222cf6b6a0dadb18ea0a78037c110
alae-herrak <alaeherrak@gmail.com>
2023-11-04 11:42:06 +0100

    changed username input type to text

--- app/ui/register-form.tsx
+++ app/ui/register-form.tsx
@@ -22,7 +22,7 @@ export default function RegisterForm() {
             <input
               className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
               id="username"
-              type="email"
+              type="text"
               name="username"
               placeholder="Choose a username"
               required
